950fdaff67137203114f850f23b82cb337bb7bf9
Adem kedir <ademkedir724@gmail.com>
2025-07-08 16:32:46 +0300

    add assignto ui

--- app/(dashboard)/(routes)/incidents/_components/IncidentsList.tsx
+++ app/(dashboard)/(routes)/incidents/_components/IncidentsList.tsx
@@ -12,6 +12,9 @@ import { formatDistanceToNow } from "date-fns";
 import { useUser } from "@clerk/nextjs";
 import { IncidentSkeleton } from "./IncidentSkeleton";
 import { Dropdown } from "./DropDown";
+import SelectItems from "../new/_components/SelectItems";
+import { useState } from "react";
+import { Department, IncidentCategory } from "@prisma/client";
 
 function getBadgeVariantForStatus(status: string) {
   switch (status) {
@@ -28,51 +31,94 @@ function getBadgeVariantForStatus(status: string) {
   }
 }
 
-export default function IncidentsList({ incidents }: { incidents: any[] }) {
-  const { isLoaded, user } = useUser();
+function IncidentItem({
+  incident,
+  currentUser,
+  users,
+}: {
+  // incident: Incident & { department: Department; category: IncidentCategory };
+  incident: any;
+  currentUser: any;
+  users: { name: string; id: string }[];
+}) {
+  const [userId, setUserId] = useState(incident.assignedToId || "");
+  console.log({ userId });
+  return (
+    <Card
+      key={incident.id}
+      className="rounded-2xl border border-muted bg-background shadow-sm hover:shadow-lg   transition-shadow"
+    >
+      <CardHeader>
+        <CardTitle className="flex justify-between items-center">
+          {incident.title}
+          <Badge
+            variant={getBadgeVariantForStatus(incident.status)}
+            className="px-4 py-2"
+          >
+            <span>{incident.status}</span>
+            {currentUser?.publicMetadata.position === "higher" && (
+              <Dropdown status={incident.status} incidentId={incident.id} />
+            )}
+          </Badge>
+        </CardTitle>
+        <CardDescription className="text-sm text-muted-foreground">
+          Reported {formatDistanceToNow(new Date(incident.createdAt))} ago{" "}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        {incident.description && (
+          <p className="text-sm">{incident.description}</p>
+        )}
+        <div className="text-sm text-muted-foreground">
+          <strong>Location:</strong> {incident.location || "N/A"}
+        </div>
+        <div className="text-sm text-muted-foreground">
+          <strong>Category:</strong> {incident.category?.name}
+        </div>
+        <div className="text-sm text-muted-foreground">
+          <strong>Department:</strong> {incident.department?.name}
+        </div>
+
+        {incident.assignedToId ? (
+          <div className="text-sm text-muted-foreground">
+            <strong>Assigned to:</strong> {incident.assignedToId}
+          </div>
+        ) : (
+          <SelectItems
+            label="Assign to Staff"
+            value={userId}
+            items={users}
+            onChange={setUserId}
+          />
+        )}
+      </CardContent>
+    </Card>
+  );
+}
+
+export default function IncidentsList({
+  incidents,
+  users,
+}: {
+  incidents: any;
+  users: { name: string; id: string }[];
+}) {
+  const { isLoaded, user: currentUser } = useUser();
+  // const [userId, setUserId] = useState(incident.department?.AssignedToId);
 
   if (!isLoaded) {
     return <IncidentSkeleton />;
   }
+
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 overflow-x-auto">
-      {incidents.map((incident) => (
-        <Card
+      {incidents.map((incident: any) => (
+        <IncidentItem
           key={incident.id}
-          className="rounded-2xl border border-muted bg-background shadow-sm hover:shadow-lg   transition-shadow"
-        >
-          <CardHeader>
-            <CardTitle className="flex justify-between items-center">
-              {incident.title}
-              <Badge
-                variant={getBadgeVariantForStatus(incident.status)}
-                className="px-4 py-2"
-              >
-                <span>{incident.status}</span>
-                {user?.publicMetadata.position === "higher" && (
-                  <Dropdown status={incident.status} incidentId={incident.id} />
-                )}
-              </Badge>
-            </CardTitle>
-            <CardDescription className="text-sm text-muted-foreground">
-              Reported {formatDistanceToNow(new Date(incident.createdAt))} ago{" "}
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            {incident.description && (
-              <p className="text-sm">{incident.description}</p>
-            )}
-            <div className="text-sm text-muted-foreground">
-              <strong>Location:</strong> {incident.location || "N/A"}
-            </div>
-            <div className="text-sm text-muted-foreground">
-              <strong>Category:</strong> {incident.category?.name}
-            </div>
-            <div className="text-sm text-muted-foreground">
-              <strong>Department:</strong> {incident.department?.name}
-            </div>
-          </CardContent>
-        </Card>
+          incident={incident}
+          currentUser={currentUser}
+          users={users}
+        />
       ))}
     </div>
   );
--- app/(dashboard)/(routes)/incidents/page.tsx
+++ app/(dashboard)/(routes)/incidents/page.tsx
@@ -1,42 +1,7 @@
-// import { prisma } from "@/app/lib/prisma";
-// import { auth, currentUser } from "@clerk/nextjs/server";
-// import { redirect } from "next/navigation";
-
-// async function page() {
-//   // const { userId } = await auth();
-
-//   const user = await currentUser();
-//   console.log("jlk", { user });
-//   if (!user?.id) {
-//     redirect("/");
-//   }
-
-//   const currentUserRole = user.publicMetadata.role;
-//   const currentUserDepId = user.publicMetadata.departmentId;
-//   console.log({ currentUserDepId, currentUserRole });
-//   let incidents;
-//   if (currentUserRole === "admin") {
-//     incidents = await prisma.incident.findMany({});
-//   } else {
-//     incidents = await prisma.incident.findMany({
-//       where: {
-//         departmentId: `${currentUserDepId}`,
-//       },
-//     });
-//   }
-//   console.log({ incidents });
-//   return (
-//     <div>
-//       <h1>This is incidents page</h1>
-//     </div>
-//   );
-// }
-
-// export default page;
-// app/incidents/page.tsx
-
 import { prisma } from "@/app/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
+import { createClerkClient } from "@clerk/backend";
+
 import { redirect } from "next/navigation";
 import IncidentsList from "./_components/IncidentsList";
 
@@ -65,11 +30,36 @@ export default async function IncidentsPage() {
       orderBy: { createdAt: "desc" },
     });
   }
+  const clerkClient = createClerkClient({
+    secretKey: process.env.CLERK_SECRET_KEY,
+  });
 
+  const { data, totalCount } = await clerkClient.users.getUserList({
+    orderBy: "-created_at",
+    limit: 500,
+  });
+
+  const users = data
+    .filter((user) => user.publicMetadata.departmentId === currentUserDepId)
+    .map((user) => {
+      return { name: user?.fullName || "", id: user.id };
+    });
+  console.log({ users });
+
+  // const onAssignUser = async (incidentId: string, userId: string) => {
+  //   await prisma.incident.update({
+  //     where: {
+  //       id: incidentId,
+  //     },
+  //     data: {
+  //       assignedToId: userId,
+  //     },
+  //   });
+  // };
   return (
     <div className="container p-8">
       <h1 className="text-3xl font-bold mb-6">Incident Reports</h1>
-      <IncidentsList incidents={incidents} />
+      <IncidentsList incidents={incidents} users={users} />
     </div>
   );
 }
